13b039a1ee05fe0a209ceeed2ae80b7663d81a07
Manila Grg <manilagrg@gmail.com>
2019-12-23 04:58:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,7 @@
 function basicTeenager(age) {
-
+  if (age == 13-19){
+    return "You are a teenager!"
+  }
 }
 
 function teenager(age) {
@@ -17,3 +19,5 @@ function ternaryTeenager(age) {
 function switchAge(age) {
 
 }
+
+basicTeenager()

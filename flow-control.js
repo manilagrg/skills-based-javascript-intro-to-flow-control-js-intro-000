0f5bd675d7fbc75da74e4a27bef93f2c961c134b
Manila Grg <manilagrg@gmail.com>
2019-12-23 05:45:42 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -26,9 +26,9 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  if(age >= 13 && age <= 19){
+  return age >= 13 && age <= 19
      "You are a teenager" : "You are not a teenager"
-  }
+  
 }
 
 function switchAge(age) {
